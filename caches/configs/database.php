d3f951b1bc27ccb8416f77432f9e6c0fede6c730
cfp <u400@qq.com>
2011-10-08 20:38:59 +0800

    test

---
+++ caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'diandongche',
+		'username' => 'root',
+		'password' => '123456',
+		'tablepre' => 'v9_',
+		'charset' => 'gbk',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ caches/configs/version.php
@@ -0,0 +1,4 @@
+<?php return array (
+  'pc_version' => 'V9.1.5',
+  'pc_release' => '20110705',
+);?>
